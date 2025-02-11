5a80f1e8af989bcc09c4edd9d20ffde82c09c1a0
Yusfdemir <demirysf4@gmail.com>
2025-02-11 17:02:29 +0300

    ders 1 kodları

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['firebasestorage.googleapis.com','st-troy.mncdn.com'],
+  },
+};
+
+export default nextConfig;
