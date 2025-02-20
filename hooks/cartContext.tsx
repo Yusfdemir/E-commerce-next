ed8155221972cdd5504c2aaa5293be0efd88b81b
Yusfdemir <demirysf4@gmail.com>
2025-02-20 16:31:42 +0300

    ders 4 kodları

--- hooks/cartContext.tsx
+++ hooks/cartContext.tsx
@@ -6,8 +6,11 @@ import toast from "react-hot-toast";
 interface CartContextProps{
     productCartQty:number
     addToBasket:(product:CardProductProps)=>void;
+    addToBasketIncrease:(product:CardProductProps)=>void;
+    addToBasketDecrease:(product:CardProductProps)=>void;
     removeFromCart:(product:CardProductProps)=>void;
-    cartPrdcts:CardProductProps[] | null
+    cartPrdcts:CardProductProps[] | null;
+    removeCart:()=>void;
 }
 
 const CartContext =createContext<CartContextProps | null>(null)
@@ -26,6 +29,44 @@ export const CartContextProvider = (props:Props)=>{
       let getItemParse:CardProductProps[] | null = JSON.parse(getItem)
       setCartPrdcts(getItemParse)
     }, [])
+
+    const addToBasketIncrease = useCallback((product:CardProductProps)=>{
+        let updatedCart;
+        if(product.quantity == 10){
+            return toast.error("Daha fazla ekleyemezsin..")
+        }
+        if(cartPrdcts){
+            updatedCart=[...cartPrdcts]
+            const existingItem= cartPrdcts.findIndex(item => item.id === product.id)
+            if(existingItem > -1){
+                updatedCart[existingItem].quantity = ++updatedCart[existingItem].quantity
+            }
+            setCartPrdcts(updatedCart)
+            localStorage.setItem("cart",JSON.stringify(updatedCart))
+        }
+    },[cartPrdcts])
+
+    const addToBasketDecrease = useCallback((product:CardProductProps)=>{
+        let updatedCart;
+        if(product.quantity == 1){
+            return toast.error("Daha az ekleyemezsin..")
+        }
+        if(cartPrdcts){
+            updatedCart=[...cartPrdcts]
+            const existingItem= cartPrdcts.findIndex(item => item.id === product.id)
+            if(existingItem > -1){
+                updatedCart[existingItem].quantity = --updatedCart[existingItem].quantity
+            }
+            setCartPrdcts(updatedCart)
+            localStorage.setItem("cart",JSON.stringify(updatedCart))
+        }
+    },[cartPrdcts])
+
+    const removeCart = useCallback(()=>{
+        setCartPrdcts(null)
+        toast.success("Sepet Temizlendi...")
+        localStorage.setItem("cart",JSON.stringify(null))
+    },[])
     
     const addToBasket = useCallback((product:CardProductProps)=>{
         setCartPrdcts(prew => {
@@ -42,14 +83,22 @@ export const CartContextProvider = (props:Props)=>{
     },[cartPrdcts])
 
     const removeFromCart = useCallback((product:CardProductProps)=>{
-        
-    },[])
+        if(cartPrdcts){
+            const filteredProducts = cartPrdcts.filter(cart=> cart.id !== product.id )
+            setCartPrdcts(filteredProducts)
+            toast.success("Ürün Sepeteten Silindi...")
+            localStorage.setItem("cart",JSON.stringify(filteredProducts))
+        }
+    },[cartPrdcts])
     
     let value= {
     productCartQty,
     addToBasket,
     removeFromCart,
-    cartPrdcts
+    cartPrdcts,
+    removeCart,
+    addToBasketIncrease,
+    addToBasketDecrease
     }
 
     return (
